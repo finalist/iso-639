609b2c6b3aa25ebaedcfefcb216dfe8f1ac7b0ca
Arie <git@ariekanarie.nl>
2013-08-19 14:51:10 +0200

    use local names instead of french names

--- test/test_ISO_639.rb
+++ test/test_ISO_639.rb
@@ -2,39 +2,33 @@ require 'helper'
 
 class TestISO639 < Test::Unit::TestCase
   
-  should "have full code list in ISO_639_2" do
-    assert_equal 485, ISO_639::ISO_639_2.length
-  end
-  
   should "have shorter code list in ISO_639_1" do
     assert_equal 184, ISO_639::ISO_639_1.length
   end
   
   should "return entry for alpha-2 code" do
-    assert_equal ["eng", "", "en", "English", "anglais"], ISO_639.find_by_code("en")
-    assert_equal ["eng", "", "en", "English", "anglais"], ISO_639.find("en")
+    assert_equal ["eng", "en", "English", "English"], ISO_639.find_by_code("en")
+    assert_equal ["eng", "en", "English", "English"], ISO_639.find("en")
   end
   
   should "find by english name" do
-    assert_equal ["eng", "", "en", "English", "anglais"], ISO_639.find_by_english_name("English")
+    assert_equal ["eng", "en", "English", "English"], ISO_639.find_by_english_name("English")
   end
   
-  should "find by french name" do
-    assert_equal ["eng", "", "en", "English", "anglais"], ISO_639.find_by_french_name("anglais")
+  should "find by local name" do
+    assert_equal ["dut", "nl", "Dutch", "Nederlands, Vlaams"], ISO_639.find_by_local_name("Nederlands, Vlaams")
   end
   
   %w[
     alpha3_bibliographic
-    alpha3
-    alpha3_terminologic
     alpha2
     english_name
-    french_name
+    local_name
   ].each_with_index do |m, i|
     should "respond to and return #{m}" do
       @entry = ISO_639.find("en")
       assert @entry.respond_to?(m)
-      assert_equal ["eng", "eng", "", "en", "English", "anglais"][i], @entry.send(m)
+      assert_equal ["eng", "en", "English", "English"][i], @entry.send(m)
     end
   end
   
